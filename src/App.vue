526708de184550469683571c2be535f19d8e9ec2
Tristan Kirwan <0944035@hr.nl>
2021-09-11 22:36:17 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <Navbar />
+  <router-view/>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+:root {
+  --color-green: #1DB954;
+  --color-black: #191414;
+  --color-white: #fff;
+  --color-grey: #2c394b25;
+
+  --font-sm: 12px;
+  --font-md: 14px;
+  --font-lg: 16px;
+
+}
+
+html {
+  font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
+    Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif,
+    Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+    background-color: var(--color-white);
+    color: var(--color-black)
+}
+
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav id="nav">
+    <div class="siteTitle">SpotifAPI<i class="fab fa-spotify"></i></div>
+    <div class="searchBar">
+      <i class="fas fa-search"></i>
+    </div>
+    <hr>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@import '@/styles/imports.scss';
+nav {
+  display: grid;
+  grid-gap: 15px;
+  grid-template-columns: 3fr 1fr;
+  padding: 0 10px 25px;
+  max-width: 1200px;
+  margin: 0 auto;
+  align-items: center;
+}
+
+hr {
+  width: 100%;
+  margin: 0 auto;
+  border-bottom: 1px solid var(--color-grey);
+  grid-column: 1 / -1;
+  @include m {
+    width: 90%;
+  }
+}
+
+.siteTitle {
+  font-weight: 500;
+  font-size: 2rem;
+}
+
+.fa-spotify { 
+  margin-left: 5px;
+  color: var(--color-green);
+}
+
+.searchBar {
+  justify-self: end;
+}
+</style>
